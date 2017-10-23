c9c7a9569e7d8f50b02f3708a06b48c5069db380
Aaron Ranard <aaronranard@gmail.com>
2017-10-23 13:51:45 -0600

    mass assignment issue

--- src/Models/Discussion.php
+++ src/Models/Discussion.php
@@ -11,7 +11,7 @@ class Discussion extends Model
     
     protected $table = 'chatter_discussion';
     public $timestamps = true;
-    protected $fillable = ['title', 'chatter_category_id', 'user_id', 'slug', 'color'];
+    protected $fillable = ['title', 'chatter_category_id', 'user_id', 'slug', 'color', 'last_reply_at'];
     protected $dates = ['deleted_at', 'last_reply_at'];
 
     public function user()
